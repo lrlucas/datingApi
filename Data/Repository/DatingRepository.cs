6c77a2d364c1243e1d24da61441157592e0da64d
Lucas Suarez <lucas.suarez.dev@gmail.com>
2019-10-14 00:24:41 -0400

    Added functionality Likes in user profiles

--- Data/Repository/DatingRepository.cs
+++ Data/Repository/DatingRepository.cs
@@ -36,7 +36,19 @@ namespace DatingApp.API.Data.Repository
         {
             var users = _context.Users.Include(p => p.Photos).OrderByDescending(u => u.LastActive).AsQueryable();
             users = users.Where(u => u.Id != userParams.UserId);
-//            users = users.Where(u => u.Gender == userParams.Gender)
+            //users = users.Where(u => u.Gender == userParams.Gender);
+
+            if (userParams.Likers)
+            {
+                var userLikers = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikers.Contains(u.Id));
+            }
+
+            if (userParams.Likees)
+            {
+                var userLikees = await GetUserLikes(userParams.UserId, userParams.Likers);
+                users = users.Where(u => userLikees.Contains(u.Id));
+            }
 
             if (userParams.MinAge != 18 || userParams.MaxAge != 99)
             {
@@ -60,6 +72,23 @@ namespace DatingApp.API.Data.Repository
             return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.pageSize);
         }
 
+        private async Task<IEnumerable<int>> GetUserLikes(int id, bool likers)
+        {
+            var user = await _context.Users
+                .Include(x => x.Likers)
+                .Include(x => x.Likees)
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+            if (likers)
+            {
+                return user.Likers.Where(u => u.LikeeId == id).Select(i => i.LikerId);
+            }
+            else
+            {
+                return user.Likees.Where(u => u.LikerId == id).Select(i => i.LikeeId);
+            }
+        }
+
         public async Task<User> GetUser(int id)
         {
             var user = await _context.Users
@@ -77,6 +106,12 @@ namespace DatingApp.API.Data.Repository
         public async Task<Photo> GetMainPhotoForUser(int userId)
         {
             return await _context.Photos.Where(u => u.UserId == userId).FirstOrDefaultAsync(p => p.IsMain);
+        }
+
+        public async Task<Like> GetLike(int userId, int recipientId)
+        {
+            return await _context.Likes.FirstOrDefaultAsync(u =>
+                u.LikerId == userId && u.LikeeId == recipientId);
         }
     }
 }
